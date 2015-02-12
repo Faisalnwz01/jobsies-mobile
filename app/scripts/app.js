a81c7e5501d15817b58605e1d845a20856608745
Michael J. Abraham <mjabraham@gmail.com>
2015-02-12 15:41:46 -0500

    freshm eat

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -72,15 +72,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+    // .state('tab.chat-detail', {
+    //   url: '/chats/:chatId',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'templates/chat-detail.html',
+    //       controller: 'ChatDetailCtrl'
+    //     }
+    //   }
+    // })
 
   .state('tab.friends', {
       url: '/friends',
